e8805f829897fd449351b113913639aea75d974f
Emilio Polo <emillpolog@gmail.com>
2022-12-29 22:23:39 -0800

    V0

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ const Footer = (props) => {
 
             <div className='footer-content'>
                 <img className="footer-img" src="header-img/polo.svg"/>
-                <div className='text-aligment'><h3 className='footer-text'>ⓒ 2023 All rights reserved</h3></div>
+                <div className='text-aligment'><h3 className='footer-text'>ⓒ 2023</h3></div>
                 <a className='btn-footer' href="https://www.linkedin.com/in/emilio-polo-21287313b/" target="_blank">Let's Connect</a>
             </div>   
 
