55c9eeac3fce840bd0d0e855fd1765c115dbcc7b
Emilio Polo <emillpolog@gmail.com>
2026-06-16 19:20:34 -0400

    Reorganize entries by relevance: Tier 1 (Iron Maiden, Marc Anthony, Jay Wheeler) first, Boom Operators last

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,106 +1,7 @@
 export const data =[
+
     {
         "id": 1,
-        "img":"images/1.png",
-        "title":"Dreams Adrift (Sueños al Pairo)",
-        "genre":"(Documentary)",
-        "video":"https://www.youtube.com/embed/BhrQYWjpOzc",
-        "role":"Sound Designer and Score Composer",
-        "description":"Mike Porcel is the lost member of the Cuban \"Nueva Trova\" musical movement: his lack of revolutionary spirit condemned him to the rejection of his colleagues and turned him into an outcast for a decade, until he managed to go into exile. Without resentment, but without forgetting, the film reconstructs his story and rekindles a forgotten brilliance."
-    },
-
-    {
-        "id": 2,
-        "img":"images/2.png",
-        "title":"Search On (Episode 9): Pedaling for Peace",
-        "genre":"(Documentary)",
-        "video":"https://www.youtube.com/embed/YgaWA9WkOv0",
-        "role":"Sound Mixer",
-        "description":"Inspired by Gandhi, Dnyan aims to spread peace, love, and compassion wherever he goes by meeting as many people as he can, learning from them, and sharing what he knows about the world."
-    },
-
-    {
-        "id": 3,
-        "img":"images/3.png",
-        "title":"Eyeife Festival 2019",
-        "genre":"(Documentary)",
-        "video":"https://www.youtube.com/embed/i0wmzKLIIDw",
-        "role":"Sound Mixer",
-        "description":"Eyeife Festival is an electronic music festival in Havana, Cuba. As well as bringing in a lineup of international DJs, a key aim of the festival is to shine a light on the contemporary Cuban artists that form part of the country's vibrant underground and flourishing electronic music scene."
-    },
-
-    {
-        "id": 4,
-        "img":"images/4.png",
-        "title":"Leo Brouwer on his life with music & the classical guitar",
-        "genre":"(Interview)",
-        "video":"https://www.youtube.com/embed/OCNfwkJuDFc",
-        "role":"Sound Mixer",
-        "description":" Interview with Leo Brouwer. Why does Brouwer's music stick out so distinctly from that of his contemporaries? How does the Maestro hear his music and intend it to be played? What path should young, inspired musicians take to pursue as successful a career as he has achieved?"
-    },
-
-    {
-        "id": 5,
-        "img":"images/5.png",
-        "title":"The Rodeo (El Rodeo)",
-        "genre":"(Fiction)",
-        "video":"https://www.youtube.com/embed/-pgFM-XC7Us",
-        "role":"Sound Mixer and Dialogue editor",
-        "description":"A family speaks of the past and dances with their sorrows. The morning brings a strange visitor carrying a cage."
-    },
-
-    {
-        "id": 6,
-        "img":"images/6.png",
-        "title":"The Slaughterhouse (El Matadero)",
-        "genre":"(Documentary)",
-        "video":"https://player.vimeo.com/video/667239740?h=96a8bcb1f4",
-        "role":"Sound Designer and Score Composer",
-        "description":"The Slaughterhouse presents the failure of the revolutionary program, the demagogy of his triumphalist discourse, the precariousness to which the individual's daily life is subjected as a result of the economic conditions of a society project in absolute decline."
-    },
-
-    {
-        "id": 7,
-        "img":"images/7.png",
-        "title":"Dryland (El Secadero)",
-        "genre":"(Fiction)",
-        "video":"https://www.youtube.com/embed/qtCoLZ1dzgk",
-        "role":"Sound Mixer",
-        "description":"Mario, a fourty years old man, desenchanted, is afraid for his life. A serial killer beheads his partners and slowly get near. As height, he found the seventh head; with his partner Camacho have to take ir to the station. It which seems like easy work is getting hard... the thift of the \"policial organ\" would take them to the \"El Sueco\" hunt, habanero underground king and owner of \"El Casino\"."
-    },
-
-    {
-        "id": 8,
-        "img":"images/8.png",
-        "title":"Fifth List (Lista Quinta)",
-        "genre":"(Documentary)",
-        "video":"https://www.youtube.com/embed/ukocG2MTEY8",
-        "role":"Sound Mixer and Score Composer",
-        "description":"Cuban fishermen attempt to balance their life source with environmental changes"
-    },
-
-    {
-        "id": 9,
-        "img":"https://img.youtube.com/vi/A8TytMr4g2A/hqdefault.jpg",
-        "title":"Radiate Kombucha | 30 sec Ad Spot | NBA Finals | Spotlight.online Winner",
-        "genre":"(Commercial)",
-        "video":"https://www.youtube.com/embed/A8TytMr4g2A",
-        "role":"Sound Mixer",
-        "description":"TV commercial for Radiate Kombucha aired at the NBA Finals, winners of the Spotlight.online contest. A vibrant 30-second spot capturing the brand's refreshing energy and vibrancy."
-    },
-
-    {
-        "id": 10,
-        "img":"https://img.youtube.com/vi/4q7JH1XgXNk/hqdefault.jpg",
-        "title":"Long Story Short: Alexandre Pantoja | EPISÓDIO COMPLETO | UFC",
-        "genre":"(Documentary)",
-        "video":"https://www.youtube.com/embed/4q7JH1XgXNk",
-        "role":"Sound Mixer",
-        "description":"The only current Brazilian UFC champion, Alexandre Pantoja tells his story of trials in search of recognition from his absent father — a family man ready to become world champion just to impress the woman of his life."
-    },
-
-    {
-        "id": 11,
         "img":"https://img.youtube.com/vi/BggdJLnSevQ/hqdefault.jpg",
         "title":"Iron Maiden: Burning Ambition",
         "genre":"(Documentary)",
@@ -110,27 +11,7 @@ export const data =[
     },
 
     {
-        "id": 12,
-        "img":"https://img.youtube.com/vi/KZESON4XoHI/hqdefault.jpg",
-        "title":"Cassius Ep 1 & 2",
-        "genre":"(Fiction)",
-        "video":"https://www.youtube.com/embed/KZESON4XoHI",
-        "role":"Sound Mixer",
-        "description":"CASSIUS is a bold new TV show following one man's quest to become a legendary filmmaker while navigating the dating scene in Miami — think a younger, sexier take on Curb Your Enthusiasm and Seinfeld."
-    },
-
-    {
-        "id": 13,
-        "img":"https://img.youtube.com/vi/lEMc8PLZIEQ/hqdefault.jpg",
-        "title":"Asha Puthli & Raveena Aurora Talk Musical Styles, Punjabi Sci-Fi Princesses & More | Vogue India",
-        "genre":"(Interview)",
-        "video":"https://www.youtube.com/embed/lEMc8PLZIEQ",
-        "role":"Sound Mixer",
-        "description":"Singer-songwriters Asha Puthli and Raveena Aurora talk through their musical journeys, inspirations, challenges, spirit worlds, Punjabi sci-fi princesses, resistance and racism — all while discussing the latter's newest album Asha's Awakening."
-    },
-
-    {
-        "id": 14,
+        "id": 2,
         "img":"https://img.youtube.com/vi/dPR0XlMd1Us/hqdefault.jpg",
         "title":"Marc Anthony's Muevense: Creative Conversations with Lin-Manuel Miranda",
         "genre":"(Interview)",
@@ -140,17 +21,47 @@ export const data =[
     },
 
     {
-        "id": 15,
-        "img":"https://img.youtube.com/vi/xV4pAtesbKY/hqdefault.jpg",
-        "title":"The Time is Now: Building a Healthier, Brighter Future Together",
-        "genre":"(Interview)",
-        "video":"https://www.youtube.com/embed/xV4pAtesbKY",
+        "id": 3,
+        "img":"https://img.youtube.com/vi/WRWlg2S5Uhs/hqdefault.jpg",
+        "title":"Jay Wheeler - TRAPPii Visual Album (Official Video)",
+        "genre":"(Music Video - Visual Album)",
+        "video":"https://www.youtube.com/embed/WRWlg2S5Uhs",
         "role":"Sound Mixer",
-        "description":"In Saint Rock, Haiti, providing quality health care and education are transforming lives in real-time. From the first brick of our new school campus to the rising walls of our maternal waiting home, the landscape of our community is changing for the better."
+        "description":"Official Visual Album by Jay Wheeler - \"TRAPPii\" © 2023 Linked Music / Dynamic Records / EMPIRE"
     },
 
     {
-        "id": 16,
+        "id": 4,
+        "img":"https://img.youtube.com/vi/lEMc8PLZIEQ/hqdefault.jpg",
+        "title":"Asha Puthli & Raveena Aurora Talk Musical Styles, Punjabi Sci-Fi Princesses & More | Vogue India",
+        "genre":"(Interview)",
+        "video":"https://www.youtube.com/embed/lEMc8PLZIEQ",
+        "role":"Sound Mixer",
+        "description":"Singer-songwriters Asha Puthli and Raveena Aurora talk through their musical journeys, inspirations, challenges, spirit worlds, Punjabi sci-fi princesses, resistance and racism — all while discussing the latter's newest album Asha's Awakening."
+    },
+
+    {
+        "id": 5,
+        "img":"https://img.youtube.com/vi/4q7JH1XgXNk/hqdefault.jpg",
+        "title":"Long Story Short: Alexandre Pantoja | EPISÓDIO COMPLETO | UFC",
+        "genre":"(Documentary)",
+        "video":"https://www.youtube.com/embed/4q7JH1XgXNk",
+        "role":"Sound Mixer",
+        "description":"The only current Brazilian UFC champion, Alexandre Pantoja tells his story of trials in search of recognition from his absent father — a family man ready to become world champion just to impress the woman of his life."
+    },
+
+    {
+        "id": 6,
+        "img":"https://img.youtube.com/vi/A8TytMr4g2A/hqdefault.jpg",
+        "title":"Radiate Kombucha | 30 sec Ad Spot | NBA Finals | Spotlight.online Winner",
+        "genre":"(Commercial)",
+        "video":"https://www.youtube.com/embed/A8TytMr4g2A",
+        "role":"Sound Mixer",
+        "description":"TV commercial for Radiate Kombucha aired at the NBA Finals, winners of the Spotlight.online contest. A vibrant 30-second spot capturing the brand's refreshing energy and vibrancy."
+    },
+
+    {
+        "id": 7,
         "img":"images/19.png",
         "portrait": true,
         "title":"The Sad Song of Lolita",
@@ -161,7 +72,37 @@ export const data =[
     },
 
     {
-        "id": 17,
+        "id": 8,
+        "img":"images/1.png",
+        "title":"Dreams Adrift (Sueños al Pairo)",
+        "genre":"(Documentary)",
+        "video":"https://www.youtube.com/embed/BhrQYWjpOzc",
+        "role":"Sound Designer and Score Composer",
+        "description":"Mike Porcel is the lost member of the Cuban \"Nueva Trova\" musical movement: his lack of revolutionary spirit condemned him to the rejection of his colleagues and turned him into an outcast for a decade, until he managed to go into exile. Without resentment, but without forgetting, the film reconstructs his story and rekindles a forgotten brilliance."
+    },
+
+    {
+        "id": 9,
+        "img":"images/6.png",
+        "title":"The Slaughterhouse (El Matadero)",
+        "genre":"(Documentary)",
+        "video":"https://player.vimeo.com/video/667239740?h=96a8bcb1f4",
+        "role":"Sound Designer and Score Composer",
+        "description":"The Slaughterhouse presents the failure of the revolutionary program, the demagogy of his triumphalist discourse, the precariousness to which the individual's daily life is subjected as a result of the economic conditions of a society project in absolute decline."
+    },
+
+    {
+        "id": 10,
+        "img":"images/8.png",
+        "title":"Fifth List (Lista Quinta)",
+        "genre":"(Documentary)",
+        "video":"https://www.youtube.com/embed/ukocG2MTEY8",
+        "role":"Sound Mixer and Score Composer",
+        "description":"Cuban fishermen attempt to balance their life source with environmental changes"
+    },
+
+    {
+        "id": 11,
         "img":"https://img.youtube.com/vi/GWlXRTfNw2I/hqdefault.jpg",
         "title":"Young People, Big Dreams",
         "genre":"(Short Film)",
@@ -171,17 +112,77 @@ export const data =[
     },
 
     {
-        "id": 18,
-        "img":"https://img.youtube.com/vi/WRWlg2S5Uhs/hqdefault.jpg",
-        "title":"Jay Wheeler - TRAPPii Visual Album (Official Video)",
-        "genre":"(Music Video - Visual Album)",
-        "video":"https://www.youtube.com/embed/WRWlg2S5Uhs",
-        "role":"Sound Mixer",
-        "description":"Official Visual Album by Jay Wheeler - \"TRAPPii\" © 2023 Linked Music / Dynamic Records / EMPIRE"
+        "id": 12,
+        "img":"images/5.png",
+        "title":"The Rodeo (El Rodeo)",
+        "genre":"(Fiction)",
+        "video":"https://www.youtube.com/embed/-pgFM-XC7Us",
+        "role":"Sound Mixer and Dialogue editor",
+        "description":"A family speaks of the past and dances with their sorrows. The morning brings a strange visitor carrying a cage."
     },
 
     {
-        "id": 23,
+        "id": 13,
+        "img":"images/2.png",
+        "title":"Search On (Episode 9): Pedaling for Peace",
+        "genre":"(Documentary)",
+        "video":"https://www.youtube.com/embed/YgaWA9WkOv0",
+        "role":"Sound Mixer",
+        "description":"Inspired by Gandhi, Dnyan aims to spread peace, love, and compassion wherever he goes by meeting as many people as he can, learning from them, and sharing what he knows about the world."
+    },
+
+    {
+        "id": 14,
+        "img":"images/3.png",
+        "title":"Eyeife Festival 2019",
+        "genre":"(Documentary)",
+        "video":"https://www.youtube.com/embed/i0wmzKLIIDw",
+        "role":"Sound Mixer",
+        "description":"Eyeife Festival is an electronic music festival in Havana, Cuba. As well as bringing in a lineup of international DJs, a key aim of the festival is to shine a light on the contemporary Cuban artists that form part of the country's vibrant underground and flourishing electronic music scene."
+    },
+
+    {
+        "id": 15,
+        "img":"images/4.png",
+        "title":"Leo Brouwer on his life with music & the classical guitar",
+        "genre":"(Interview)",
+        "video":"https://www.youtube.com/embed/OCNfwkJuDFc",
+        "role":"Sound Mixer",
+        "description":" Interview with Leo Brouwer. Why does Brouwer's music stick out so distinctly from that of his contemporaries? How does the Maestro hear his music and intend it to be played? What path should young, inspired musicians take to pursue as successful a career as he has achieved?"
+    },
+
+    {
+        "id": 16,
+        "img":"images/7.png",
+        "title":"Dryland (El Secadero)",
+        "genre":"(Fiction)",
+        "video":"https://www.youtube.com/embed/qtCoLZ1dzgk",
+        "role":"Sound Mixer",
+        "description":"Mario, a fourty years old man, desenchanted, is afraid for his life. A serial killer beheads his partners and slowly get near. As height, he found the seventh head; with his partner Camacho have to take ir to the station. It which seems like easy work is getting hard... the thift of the \"policial organ\" would take them to the \"El Sueco\" hunt, habanero underground king and owner of \"El Casino\"."
+    },
+
+    {
+        "id": 17,
+        "img":"https://img.youtube.com/vi/KZESON4XoHI/hqdefault.jpg",
+        "title":"Cassius Ep 1 & 2",
+        "genre":"(Fiction)",
+        "video":"https://www.youtube.com/embed/KZESON4XoHI",
+        "role":"Sound Mixer",
+        "description":"CASSIUS is a bold new TV show following one man's quest to become a legendary filmmaker while navigating the dating scene in Miami — think a younger, sexier take on Curb Your Enthusiasm and Seinfeld."
+    },
+
+    {
+        "id": 18,
+        "img":"https://img.youtube.com/vi/xV4pAtesbKY/hqdefault.jpg",
+        "title":"The Time is Now: Building a Healthier, Brighter Future Together",
+        "genre":"(Interview)",
+        "video":"https://www.youtube.com/embed/xV4pAtesbKY",
+        "role":"Sound Mixer",
+        "description":"In Saint Rock, Haiti, providing quality health care and education are transforming lives in real-time. From the first brick of our new school campus to the rising walls of our maternal waiting home, the landscape of our community is changing for the better."
+    },
+
+    {
+        "id": 19,
         "img":"https://img.youtube.com/vi/La-gWQNcqS4/hqdefault.jpg",
         "title":"Material 35 Años Jazz Plaza",
         "genre":"(Documentary - Promotional)",
@@ -191,7 +192,7 @@ export const data =[
     },
 
     {
-        "id": 22,
+        "id": 20,
         "img":"https://img.youtube.com/vi/ozHGRL4TfYk/hqdefault.jpg",
         "title":"Michelangelo Pistoletto: El tercer paraíso - Performance at 12th Havana Biennial",
         "genre":"(Promotional)",
@@ -201,7 +202,7 @@ export const data =[
     },
 
     {
-        "id": 19,
+        "id": 21,
         "img":"https://img.youtube.com/vi/VSFXhakiFCw/hqdefault.jpg",
         "title":"Know Me",
         "genre":"(Drama)",
@@ -211,7 +212,7 @@ export const data =[
     },
 
     {
-        "id": 20,
+        "id": 22,
         "img":"https://img.youtube.com/vi/P_yeWY0b_RE/hqdefault.jpg",
         "title":"Ethan Bloom",
         "genre":"(Comedy-Drama)",
@@ -221,7 +222,7 @@ export const data =[
     },
 
     {
-        "id": 21,
+        "id": 23,
         "img":"https://img.youtube.com/vi/drJD43vgLnM/hqdefault.jpg",
         "title":"Mountains",
         "genre":"(Drama)",
